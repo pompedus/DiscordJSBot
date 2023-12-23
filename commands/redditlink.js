fc93454d57ff037db58035c0f1208bcaa3c2c6d0
Pompedus <pompedus@proton.me>
2023-12-23 17:03:28 +0100

    Rewritten the fetch flow to use try/catch instead of chaining "thens"

--- commands/redditlink.js
+++ commands/redditlink.js
@@ -15,40 +15,40 @@ module.exports = {
         let attempts = 0;
         const maxAttempts = 5;
 
-        async function fetchJson() {
-            fetch(jsonUrl)
-                .then(response => {
-                    if (response.headers.get("content-type").includes("application/json")) {
-                        return response.json();
-                    } else {
-                        throw new Error("Received HTML instead of JSON");
-                    }
-                })
-                .then(async (json) => {
-                    var data = json[0].data.children[0].data
-                    switch (data.is_video) {
-                        case true:
-                            await interaction.reply(data.media.reddit_video.fallback_url);
-                            console.log(new Date().toLocaleString() + ": " + "User " + interaction.user.username + " used command " + interaction.commandName + " to send a video successfully!")
-                            return
-                        case false:
-                            await interaction.reply(data.url);
-                            console.log(new Date().toLocaleString() + ": " + "User " + interaction.user.username + " used command " + interaction.commandName + " to send a gif successfully!")
-                            return
-                    }
-                })
-                .catch(async error => {
-                    console.log(new Date().toLocaleString() + ": " + "User " + interaction.user.username + " used command " + interaction.commandName + " unsuccessfully.");
-                    if (error.message === "Received HTML instead of JSON" && attempts < maxAttempts) {
-                        attempts++;
-                        console.log(new Date().toLocaleString() + ": " + "Fail " + attempts + "/" + maxAttempts + " to fetch JSON - retrying.");
-                        fetchJson();
-                    } else {
-                        console.log(new Date().toLocaleString() + ": " + error);
-                        await interaction.reply({ content: "Unexpected error ocurred!\nError message is:\n**" + error + "**", ephemeral: true });
-                    }
-                });
+        async function fetchJson(url) {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`${response.status}`);
+                }
+                const json = await response.json();
+                return json;
+            } catch (error) {
+                console.log(new Date().toLocaleString() + ": " + "User " + interaction.user.username + " used command " + interaction.commandName + " unsuccessfully.");
+                if (error.message === '403' && attempts < maxAttempts) {
+                    attempts++;
+                    console.log(new Date().toLocaleString() + ": " + error + " - Try: " + attempts + "/" + maxAttempts + " to fetch JSON - Retrying.");
+                    return fetchJson(url);
+                } else {
+                    console.log(new Date().toLocaleString() + ": " + error);
+                    throw error;
+                }
+            }
         }
-        fetchJson();
+        await fetchJson(jsonUrl).then(json => {
+            var data = json[0].data.children[0].data
+            switch (data.is_video) {
+                case true:
+                    interaction.reply(data.media.reddit_video.fallback_url);
+                    console.log(new Date().toLocaleString() + ": " + "User " + interaction.user.username + " used command " + interaction.commandName + " to send a video successfully.");
+                    break;
+                case false:
+                    interaction.reply(data.url);
+                    console.log(new Date().toLocaleString() + ": " + "User " + interaction.user.username + " used command " + interaction.commandName + " to send a gif successfully.");
+                    break;
+            }
+        }).catch(error => {
+            interaction.reply({ content: "Unexpected error ocurred!\n**" + error + "**", ephemeral: true });
+        })
     }
 }
