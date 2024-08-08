1a350ccc7b2a4b2a0eb7a79debf9dd032774350c
Pontus J <pompedus@proton.me>
2024-08-08 20:58:37 +0200

    Changed model to allow more RPD

--- commands/gpt.js
+++ commands/gpt.js
@@ -5,7 +5,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.GPT_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 function splitStringByLength(str, length) {
     let result = [];
